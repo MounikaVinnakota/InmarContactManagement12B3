9d3dcfea0919e4077b107d28df524b3c87a64806
MounikaVinnakota <36676044+MounikaVinnakota@users.noreply.github.com>
2018-02-21 23:00:36 +0530

    Update login.php

--- login.php
+++ login.php
@@ -12,11 +12,12 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$mail2="";
-$passwd2="";
 $mail1=$_POST['email'];
 $passwd1=$_POST['pass'];
-   echo "$mail1    $passwd1";
+$mail2="";
+$passwd2="";
+
+  // echo "$mail1    $passwd1";
 
 $sql = "SELECT Email,Password FROM persons where Email='$mail1'";
 $result = $conn->query($sql);
@@ -26,17 +27,18 @@ if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
       $mail2=$row["Email"];
       $passwd2=$row["Password"];
+      //echo "$mail2  $passwd2";
       }
 } 
 else {
 
     echo "0 results";
 }
-echo "<br>$mail2   $passwd2";
+//echo "<br>$mail2   $passwd2";
    if(($mail1==$mail2)&&($passwd1==$passwd2))
    {
-       setcookie("Email","$mail2", time() + (86400 * 30), "/");
-       setcookie("test2","x2", time() + (86400 * 30), "/");
+       setcookie("test","x" ,time() + (86400 * 30), "/");
+       setcookie("Email",$mail2, time() + (86400 * 30), "/");
        //echo "<h1>This is fine</h1>";
        header("Location:index.php");
    }
